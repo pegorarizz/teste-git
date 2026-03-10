ce794f7c79ae66442689b54ce5fc9e6ddcd73f75
Aluno <joaopegorari9@gmail.com>
2026-03-10 09:37:55 -0300

    Verifica o número

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-n = input("Digite um numero:")
-print(n)
+while True:
+    try:
+        numero = int(input("Digite um número: "))
+        break
+    except ValueError:
+        print("Você digitou algo inválido. Digite apenas números.")
+        
+print(numero + 2)
+while True:
+    try:
+        n = int(input("Digite um número para calcular o fatorial: "))
+        break
+    except ValueError:
+        print("Digite apenas números.")
+
+fatorial = 1
+
+for i in range(1, n + 1):
+    fatorial *= i
+
+print(f"O fatorial de {n} é {fatorial}")
